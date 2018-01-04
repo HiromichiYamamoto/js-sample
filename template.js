6da74b783a551ba463f9ed9a6724b1e1c387e356
HiromichiYamamoto <>
2018-01-04 19:59:16 +0900

    operator

--- template.js
+++ template.js
@@ -6,3 +6,8 @@ console.log(str)
 var obj = { x:1, y:2, z:3 };
 console.log(obj.x);
 console.log(obj['x']);
+
+console.log(10 + 1);
+console.log('10' + 1);
+var today = new Date();
+console.log(1234 + today);
