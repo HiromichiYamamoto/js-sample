cd476208d8fb835dc61319e7f88ced8d885da882
HiromichiYamamoto <>
2018-01-04 23:56:50 +0900

    replace

--- equal.js
+++ equal.js
@@ -1,4 +1,4 @@
-// 基本型と参照型による代入の違い
+// ？
 var x = 1;
 var y = x;
 x = 2;
@@ -8,3 +8,16 @@ var data1 = [0,1,2];
 var data2 = data1;
 data1[0] = 5;
 console.log(data2);
+
+// 配列の分割代入
+let data = [56,49,23,50,13,23,45,90];
+let [x0,x1,x2,...other] = data
+console.log(x0);
+console.log(other);
+
+// デフォルトでなしを設定
+let book = {title: 'Javaポケットリファレンス', publish: '技術評論社', price: 2000};
+let {price, title,memo = 'なし'} = book;
+console.log(title);
+console.log(price);
+console.log(memo);
