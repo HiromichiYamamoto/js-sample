62be9163a92f235cf3ed1fad18a0e749a402b309
HiromichiYamamoto <fig@Fig.local>
2018-01-09 17:05:24 +0900

    add dom

--- element_id.js
+++ element_id.js
@@ -1,3 +1,9 @@
-var current = new Date();
-var result = document.getElementById('result');
-result.textContent = current.toLocalString();
+var s = document.getElementById('food');
+var opts = s.childNodes;
+
+for (var i = 0, ken = opts.length; i < len; i++) {
+  var opt = opts.item(i);
+  if (opts.nodeType === 1) {
+    console.log(opt.value);
+  }
+}
