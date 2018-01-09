252f081c45856a4a4f4c42de1a1709bc470f4a76
HiromichiYamamoto <>
2018-01-09 09:40:48 +0900

    add function

--- function.js
+++ function.js
@@ -58,3 +58,26 @@ function showElement(value,key) {
 
 var ary = [1,2,3,4,5];
 arrayWalk(ary,showElement);
+
+//初期化処理
+var Member = function(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.getName = function() {
+    return this.lastName + ' ' + this.firstName;
+  }
+};
+var mem = new Member('洋二', '山田');
+console.log(mem.getName());
+
+// call
+var data = 'Global data';
+var obj1 = { data: 'obj1 data'};
+var obj2 = { data: 'obj2 data'};
+
+function hoge() {
+  console.log(this.data);
+}
+hoge.call(null);
+hoge.call(obj1);
+hoge.call(obj2);
